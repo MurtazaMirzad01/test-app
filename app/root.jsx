c35788a2281f6f5ba9b46dc2db710cd42fb61955
Mirzad01 <murtazamasomi@gmail.com>
2025-11-06 11:56:34 +0430

    intial commit

---
+++ app/root.jsx
@@ -0,0 +1,26 @@
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="preconnect" href="https://cdn.shopify.com/" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
+        />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <h1>Hello from test app</h1>
+        <h3>This is my test app</h3>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
